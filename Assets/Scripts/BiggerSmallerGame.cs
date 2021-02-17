e34fe66e1875ec0c7df954743519f5a4ecf50865
OmegaP1 <J.veloso.00@gmail.com>
2021-02-17 12:27:04 +0000

    Game

---
+++ Assets/Scripts/BiggerSmallerGame.cs
@@ -0,0 +1,227 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BiggerSmallerGame : MonoBehaviour
+{
+    public Image card;
+    public Sprite[] DeckOfCards;
+    public Text FinalText;
+    public Text PlayerName;
+
+    public GameObject UpArrow;
+    public GameObject DownArrow;
+    public GameObject EqualsButton;
+    public GameObject PickACardButton;
+    public GameObject CardBack;
+    public GameObject Card;
+    public GameObject[] Beers;
+
+
+    public Text[] PlayersList;
+    public List<string> ActivePlayers;
+    public GameObject CanvasPlayer;
+    public GameObject CanvasGame;
+
+    private int CardGenarated;
+    private int NumberOFBeer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GeneratePlayers();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+    }
+
+    public void ChangeCard()
+    {
+        GeneratePlayers();
+        GenerateNumberOfDrinks();
+        UpArrow.SetActive(true);
+        DownArrow.SetActive(true);
+        EqualsButton.SetActive(true);
+        PickACardButton.SetActive(false);
+
+        Card.SetActive(true);
+        CardBack.SetActive(false);
+
+        CardChange();
+        
+    }
+
+    public void EqualsButtonPressed()
+    {
+        int helperBeer = NumberOFBeer;
+        int helperCard = ValidateCard(CardGenarated);
+        CardChange();
+        GenerateNumberOfDrinks();
+        GeneratePlayers();
+        int newhelperCard = ValidateCard(CardGenarated);
+        print(helperCard);
+        print(newhelperCard);
+        if (helperCard == newhelperCard)
+        {
+            FinalText.color = Color.green;
+            FinalText.text = "U Win - Congratz";
+        }
+        else
+        {
+            FinalText.color = Color.red;
+            FinalText.text = "U lost - Drink   " + (helperBeer + 1) + " sips";
+        }
+    }
+
+    public void UpButtonPressed()
+    {
+        int helperBeer = NumberOFBeer;
+        int helperCard = ValidateCard(CardGenarated);
+        CardChange();
+        GenerateNumberOfDrinks();
+        GeneratePlayers();
+        int newhelperCard = ValidateCard(CardGenarated);
+        print(helperCard);
+        print(newhelperCard);
+        if (helperCard < newhelperCard)
+        {
+            FinalText.color = Color.green;
+            FinalText.text = "U Win - Congratz";
+        }
+        else
+        {
+            FinalText.color = Color.red;
+            FinalText.text = "U lost - Drink   " + (helperBeer + 1) + " sips";
+        }
+    }
+    public void DownButtonPressed()
+    {
+        int helperBeer = NumberOFBeer;
+        int helperCard = ValidateCard(CardGenarated);
+        CardChange();
+        GenerateNumberOfDrinks();
+        GeneratePlayers();
+        int newhelperCard = ValidateCard(CardGenarated);
+        print(helperCard);
+        print(newhelperCard);
+        if (helperCard > newhelperCard)
+        {
+            FinalText.color = Color.green;
+            FinalText.text = "U Win - Congratz";
+        }
+        else
+        {
+            FinalText.color = Color.red;
+            FinalText.text = "U lost - Drink   " + (helperBeer + 1) + " sips";
+        }
+    }
+
+    public void PlayBiggerSmaller()
+    {
+        PlayersVerification();
+        CanvasGame.SetActive(true);
+        CanvasPlayer.SetActive(false);
+    }
+
+    public void PlayersVerification()
+    {
+
+        for (int i = 0, b = 0; i < PlayersList.Length; i++)
+        {
+
+            if (PlayersList[i].text == "")
+            {
+
+            }
+            else
+            {
+                ActivePlayers.Add(PlayersList[i].text);
+                b++;
+            }
+
+        }
+    }
+
+
+    public void GeneratePlayers()
+    {
+        PlayerName.text = ActivePlayers[Random.Range(0,ActivePlayers.Count)];
+    }
+
+
+    void CardChange()
+    {
+        CardGenarated = Random.Range(0, 52);
+        card.sprite = DeckOfCards[CardGenarated];
+
+    }
+    void GenerateNumberOfDrinks()
+    {
+        NumberOFBeer = Random.Range(0, 5);
+        switch (NumberOFBeer)
+        {
+            case 0:
+                SetBeers(true, false, false, false, false);
+                break;
+            case 1:
+                SetBeers(true, true, false, false, false);
+                break;
+            case 2:
+                SetBeers(true, true, true, false, false);
+                break;
+            case 3:
+                SetBeers(true, true, true, true, false);
+                break;
+            case 4:
+                SetBeers(true, true, true, true, true);
+                break;
+        }
+    }
+
+    void SetBeers(bool a,bool b,bool c,bool d, bool e)
+    {
+        Beers[0].SetActive(a);
+        Beers[1].SetActive(b);
+        Beers[2].SetActive(c);
+        Beers[3].SetActive(d);
+        Beers[4].SetActive(e);
+    }
+    int ValidateCard(int card)
+    {
+        if (card == 0 || card == 1 || card == 2 || card == 3)
+            return 2;
+        else if (card == 4 || card == 5 || card == 6 || card == 7)
+            return 3;
+        else if (card == 8 || card == 18 || card == 29 || card == 40)
+            return 4;
+        else if (card == 9 || card == 19 || card == 30 || card == 41)
+            return 5;
+        else if (card == 10 || card == 20 || card == 31 || card == 42)
+            return 6;
+        else if (card == 11 || card == 21 || card == 32 || card == 43)
+            return 7;
+        else if (card == 12 || card == 22 || card == 33 || card == 44)
+            return 8;
+        else if (card == 13 || card == 23 || card == 34 || card == 45)
+            return 9;
+        else if (card == 14 || card == 24 || card == 35 || card == 46)
+            return 10;
+        else if (card == 15 || card == 25 || card == 36 || card == 47)
+            return 100;
+        else if (card == 16 || card == 26 || card == 37 || card == 48)
+            return 12;
+        else if (card == 17 || card == 27 || card == 38 || card == 49)
+            return 13;
+        else if (card == 51 || card == 28 || card == 39 || card == 50)
+            return 11;
+        else
+            return 0;
+    }
+}
+  
---
+++ BSGame1.cs
@@ -0,0 +1,201 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BSGame1 : MonoBehaviour
+{
+    public Image card;
+    public Sprite[] DeckOfCards;
+
+    public GameObject UpArrow;
+    public GameObject DownArrow;
+    public GameObject EqualsButton;
+    public GameObject PickACardButton;
+    public GameObject CardBack;
+    public GameObject Card;
+    public GameObject[] Beers;
+
+    private int GameCard;
+    private int CardGenarated;
+    private int NewCardGenarated;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    public void ChangeCard()
+    {
+        GenerateNumberOfDrinks();
+        UpArrow.SetActive(true);
+        DownArrow.SetActive(true);
+        EqualsButton.SetActive(true);
+        PickACardButton.SetActive(false);
+
+        Card.SetActive(true);
+        CardBack.SetActive(false);
+
+        CardChange();
+    }
+
+    public void EqualsButtonPressed()
+    {
+        validateCard(CardGenarated);
+        CardChange();
+        GenerateNumberOfDrinks();
+        validateCard(NewCardGenarated);
+        if (CardGenarated == NewCardGenarated)
+        {
+            //Gannho
+            print("ganhou");
+        }
+        else
+        {
+            //bebe
+            print("bebe");
+        }
+    }
+
+    public void UpButtonPressed()
+    {
+        validateCard(CardGenarated);
+        CardChange();
+        GenerateNumberOfDrinks();
+        validateCard(NewCardGenarated);
+        if (CardGenarated > NewCardGenarated)
+        {
+            //Gannho
+            print("ganhou");
+        }
+        else
+        {
+            //bebe
+            print("bebe");
+        }
+    }
+    public void DownButtonPressed()
+    {
+        validateCard(CardGenarated);
+        CardChange();
+        GenerateNumberOfDrinks();
+        validateCard(NewCardGenarated);
+        if (CardGenarated < NewCardGenarated)
+        {
+            //Gannho
+            print("ganhou");
+        }
+        else
+        {
+            //bebe
+            print("bebe");
+        }
+    }
+
+
+    void CardChange()
+    {
+        CardGenarated = Random.Range(0, 52);
+        NewCardGenarated = Random.Range(0, 52);
+        card.sprite = DeckOfCards[CardGenarated];
+
+    }
+    void GenerateNumberOfDrinks()
+    {
+        switch (Random.Range(0,5))
+        {
+            case 0:
+                SetBeers(true, false, false, false, false);
+                break;
+            case 1:
+                SetBeers(true, true, false, false, false);
+                break;
+            case 2:
+                SetBeers(true, true, true, false, false);
+                break;
+            case 3:
+                SetBeers(true, true, true, true, false);
+                break;
+            case 4:
+                SetBeers(true, true, true, true, true);
+                break;
+        }
+    }
+
+    void SetBeers(bool a,bool b,bool c,bool d, bool e)
+    {
+        Beers[0].SetActive(a);
+        Beers[1].SetActive(b);
+        Beers[2].SetActive(c);
+        Beers[3].SetActive(d);
+        Beers[4].SetActive(e);
+    }
+    void validateCard(int card)
+    {
+        switch (card)
+        {
+            case 0 or 1 or 2 or 3:
+                //2
+                GameCard = 2;
+                break;
+            case 4 or 5 or 6 or 7:
+                //3
+                GameCard = 3;
+                break;
+            case 8 or 18 or 29 or 40:
+                //4
+                GameCard = 4;
+                break;
+            case 9 or 19 or 30 or 41:
+                //5
+                GameCard = 5;
+                break;
+            case 10 or 20 or 31 or 42:
+                //6
+                GameCard = 6;
+                break;
+            case 11 or 21 or 32 or 43:
+                //7
+                GameCard = 7;
+                break;
+            case 12 or 22 or 33 or 44:
+                //8
+                GameCard = 8;
+                break;
+            case 13 or 23 or 34 or 45:
+                //9
+                GameCard = 9;
+                break;
+            case 14 or 24 or 35 or 46:
+                //10
+                GameCard = 10;
+                break;
+            case 15 or 25 or 36 or 47:
+                //Ace
+                GameCard = 100;
+                break;
+            case 16 or 26 or 37 or 48:
+                //J
+                GameCard = 12;
+                break;
+            case 17 or 27 or 38 or 49:
+                //K
+                GameCard = 13;
+                break;
+            case 51 or 28 or 39 or 50:
+                //Q
+                GameCard = 11;
+                break;
+        }
+    }
+}
+  
